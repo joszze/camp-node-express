c4935756a84adbb14f4ce71ae1f67fa3f1bb7795
joszze <jossecarlosg@gmail.com>
2022-07-28 23:39:10 -0700

    asd

--- myApp.js
+++ myApp.js
@@ -5,8 +5,8 @@ let app = express();
 
 console.log("Hello World");
 
-app.use("/",function(req,rest,next){
-    console.log(`${req.method} ${req.path} ${req.ip}`);
+app.use(function(req,rest,next){
+    console.log(req.method + " " + req.path + " - " + req.ip);
     next()
 })
 app.use("/public", express.static(__dirname + "/public"))
