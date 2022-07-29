8da4b4e1c91f94291a61a10cfbecbc4edef5bbbc
joszze <jossecarlosg@gmail.com>
2022-07-28 23:32:13 -0700

    fa

--- myApp.js
+++ myApp.js
@@ -5,12 +5,13 @@ let app = express();
 
 console.log("Hello World");
 
-app.use("/public", express.static(__dirname + "/public"))
-
 app.use("/",function(req,rest,next){
     console.log(`${req.method} ${req.path} ${req.ip}`);
     next()
 })
+app.use("/public", express.static(__dirname + "/public"))
+
+
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/views/index.html")
