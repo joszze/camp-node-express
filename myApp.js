c3bac9f903fe5a2654cf85db0e3269fc88315e55
joszze <jossecarlosg@gmail.com>
2022-07-28 23:55:23 -0700

    sa

--- myApp.js
+++ myApp.js
@@ -22,7 +22,12 @@ app.get("/json",function(req,res){
     res.json({"message" : (process.env.MESSAGE_STYLE === "uppercase" ? m.toUpperCase(): m)})
 })
 
-
+app.get('/now', function(req, res, next) {
+    req["time"] = new Date().toString();
+    next();
+  }, function(req, res) {
+    res.json({"time":req.time})
+  });
 
 
 
